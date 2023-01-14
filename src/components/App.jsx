31b1e15e4dc359fb622932aab5c8a8ecd93ae535
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-14 21:57:00 +0530

    1st commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hook from "./Hook";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Hook />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Hook.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect} from "react";
+
+function Hook() {
+    const [emotion, setEmotion] = useState("Happy");
+    useEffect(function name() {
+        console.log(`It's ${emotion} right now.`)
+    });
+    // useEffect(function name() {
+    //     console.log(`It's ${emotion} right now.`)
+    // }, []);
+    return (
+        <div>
+            <h1 className="Hook1">Current emotion is {emotion}.</h1>
+            <button className="btn1" onClick={() => setEmotion("Sad")}>Sad</button>
+            <button className="btn1" onClick={() => setEmotion("Excited")}>Excited</button>
+        </div>
+    );
+}
+
+export default Hook;
