103e948d051752d33f1d5f047e9d7eea3689f207
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-16 22:13:20 +0530

    First project done on useState and useEffect

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -3,17 +3,32 @@ import { useState, useEffect} from "react";
 
 function Hook() {
     const [emotion, setEmotion] = useState("Happy");
+    const [greating, setGreating] = useState("Hi");
     useEffect(function name() {
         console.log(`It's ${emotion} right now.`)
-    });
+    }, [emotion]);
+    useEffect(function name() {
+        console.log(`It's ${greating} right now.`)
+    }, [greating]);
+
+    // Calles only once
+
     // useEffect(function name() {
     //     console.log(`It's ${emotion} right now.`)
     // }, []);
+
+    // Calles when only emotion changes
+
+    // useEffect(function name() {
+    //     console.log(`It's ${emotion} right now.`)
+    // }, [emotion]);
     return (
         <div className="Hook-div">
             <h1 className="Hook1">Current emotion is {emotion}.</h1>
             <button className="btn1" onClick={() => setEmotion("Sad")}>Sad</button>
             <button className="btn1" onClick={() => setEmotion("Excited")}>Excited</button>
+            <h1>{greating} everyone</h1>
+            <button className="btn1" onClick={() => setGreating("Hello")}>Hello</button>
         </div>
     );
 }
