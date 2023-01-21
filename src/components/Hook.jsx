5f6c2c4441e8a920270fb9305e11b02052230226
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-21 18:57:08 +0530

    Fetching data from an API. Changes on Hook.jsx and App.css

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,46 +1,18 @@
 import "../components-css/Hook.css";
-import { useState } from "react";
-
-function useInput(initialValue) {
-    const [value, setValue] = useState(initialValue);
-    return [
-        {
-            value,
-            onChange: (e) => setValue(e.target.value)
-        },
-        () => setValue(initialValue)
-    ];
-}
+import { useState, useEffect } from "react";
 
 function Hook() {
-    const [text, setText] = useInput("");
-    const [color, setColor] = useInput("#000000");
-    const submit = (e) => {
-        e.preventDefault();
-        alert(`${text.value}, ${color.value}`);
-        setText("");
-        setColor("#000000");
-    };
-
-    return (
-        <div>
-            <form onSubmit={submit}>
-                <input 
-                    {...text}
-                    type="text"
-                    placeholder="Color Name.."
-                    required={true}
-                />
-                <input 
-                    {...color}
-                    type="color"
-                />
-                <button>
-                    Show
-                </button>
-            </form>
-        </div>
-    );
+  const [info, setInfo] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/SinhaVinit`)
+      .then((response) => response.json())
+      .then((data) => setInfo(data));
+  }, []);
+  // Remember to add [] at the end of useEffect so that API will be requested only once.
+  if (info) {
+    return <pre>{JSON.stringify(info, null, 2)}</pre>;
+  }
+  return <h1>Data</h1>;
 }
 
 export default Hook;
