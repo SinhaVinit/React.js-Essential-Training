dbd23e88d0f23ea7110f85a696a83239bb4c504b
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-21 22:07:48 +0530

    Displaying data from API. Changes in Hook.jsx.

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,6 +1,16 @@
 import "../components-css/Hook.css";
 import { useState, useEffect } from "react";
 
+function GitHubUser({ name, created_on, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{created_on}</p>
+      <img src={avatar} height={100} alt={name} />
+    </div>
+  );
+}
+
 function Hook() {
   const [info, setInfo] = useState(null);
   useEffect(() => {
@@ -10,7 +20,13 @@ function Hook() {
   }, []);
   // Remember to add [] at the end of useEffect so that API will be requested only once.
   if (info) {
-    return <pre>{JSON.stringify(info, null, 2)}</pre>;
+    return (
+      <GitHubUser
+        name={info.name}
+        created_on={info.created_at}
+        avatar={info.avatar_url}
+      />
+    );
   }
   return <h1>Data</h1>;
 }
