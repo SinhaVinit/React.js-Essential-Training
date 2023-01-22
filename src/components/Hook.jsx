2415e2fd358522bd6250adb499d88e27b6f1e091
SinhaVinit <vinit.8444935532@gmail.com>
2023-01-22 21:13:40 +0530

    Working with render props. Changes in Hook.jsx

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,58 +1,35 @@
 import "../components-css/Hook.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-    status
-  }
-}`;
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
 
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query }),
-};
-
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 function Hook() {
-  const [info, setInfo] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch(`https://snowtooth.moonhighway.com/`, opts)
-      .then((response) => response.json())
-      .then(setInfo) // .then((data) => setInfo(data))
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []); // Remember to add [] at the end of useEffect so that API will be requested only once.
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <pre>{JSON.stringify(error)}</pre>;
-  if (!info) return null;
-  console.log(info, "DATA!!");
   return (
-    <div>
-      {info.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <List
+      data={tahoe_peaks}
+      renderEmpty=<p>This list is empty</p>
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      )}
+    />
   );
 }
 
